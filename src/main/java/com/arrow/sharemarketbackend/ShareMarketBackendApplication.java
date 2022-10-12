2386a91d76c310cca1cb48eaa00b64edf84fb1d8
Rahul Kumar <rahul.verma2241@gmail.com>
2022-10-13 01:47:54 +0530

    Spring Doc addition

--- src/main/java/com/arrow/sharemarketbackend/ShareMarketBackendApplication.java
+++ src/main/java/com/arrow/sharemarketbackend/ShareMarketBackendApplication.java
@@ -1,13 +1,23 @@
 package com.arrow.sharemarketbackend;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShareMarketBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShareMarketBackendApplication.class, args);
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("springshop-public")
+                .pathsToMatch("/public/**")
+                .build();
     }
 
 }
