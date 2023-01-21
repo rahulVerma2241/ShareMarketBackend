84d18268615490b65ca6e32ecb0f474f792382bb
Rahul Kumar <rahul.verma2241@gmail.com>
2023-01-21 22:52:59 +0530

    Added test

--- src/test/java/com/arrow/sharemarketbackend/controller/SMPriceDownControllerTest.java
+++ src/test/java/com/arrow/sharemarketbackend/controller/SMPriceDownControllerTest.java
@@ -21,16 +21,15 @@ public class SMPriceDownControllerTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
-    private RequestShareModel requestShareModel;
-
     @Test
     public void givenQuantity_whenCheckForQuantity_thenReturnPrice() throws Exception {
-        requestShareModel = new RequestShareModel(42,438.0,421.6,
+        RequestShareModel  requestShareModel = new RequestShareModel(42,438.0,421.6,
                 "Tata Motors",428.0 , 66);
         ResultActions response = mockMvc.perform(get("/sharemarket/priceByQuantity")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(requestShareModel)));
-        response.andExpect(status().isOk())
-                .andDo(print());
+        response.andExpect(status().isOk()).andDo(print());
     }
+
+
 }
